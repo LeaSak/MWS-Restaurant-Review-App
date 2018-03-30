bd056f43378fbfccc45b1a62aea2b10aea1e9146
Lea-Ann Sakmann <leaann.sakmann@gmail.com>
2018-03-30 17:03:51 +0200

    fix: add caching of dynamic requests to sw

--- src/sw.js
+++ src/sw.js
@@ -1,67 +1,50 @@
 const CACHE_NAME = 'restaurant-app-v1';
 
 const STATIC_ASSETS = [
-	'/',
-	'restaurant.html',
-	'css/styles.css',
-	'js/dbhelper.js',
-	'js/main.js',
-	'js/restaurant_info.js',
-	'data/restaurants.json',
-	'https://fonts.googleapis.com/css?family=Work+Sans:400,500" rel="stylesheet'
+    '/',
+    'restaurant.html',
+    'css/styles.css',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'data/restaurants.json',
+    'https://fonts.googleapis.com/css?family=Work+Sans:400,500" rel="stylesheet'
 ];
 
 
 // Check for service worker and do sw registration
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then( registration => {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  });
 }
 
 // Cache static assets on install
 self.addEventListener('install', (event) => {
-	event.waitUntil(
-			caches.open(CACHE_NAME).then((cache) => {
-				return cache.addAll(STATIC_ASSETS);
-			}));
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(STATIC_ASSETS);
+        }));
 });
 
-// https://developers.google.com/web/fundamentals/primers/service-workers/
-self.addEventListener('fetch', event => {
-  if (event.request.url.startsWith(self.location.origin)) {
+// https://jakearchibald.com/2014/offline-cookbook/#on-network-response
+// If the request is in the cache, return it
+// else go to the network, cache the response and return it
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-      caches.match(event.request).then((response) => {
-        if (response) {
-          return response;
-        }
-        
-        // Requests are streams. Clone request to use again in fetch.
-        // Return response if ok, clone it and add it to cache.
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then((response) => {
-        	if (!response || response.status !== 200 || response.type !== 'basic'){
-        		return reponse;
-        	}
-
-        	// Responses also need to be cloned before adding it to cache.
-        	const responseToCache = response.clone();
-        	caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-
-        });
-      })
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                return response || fetch(event.request).then(function(response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
     );
-  }
 });
