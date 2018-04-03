dcdfc622316d1b46bbf450478dc2c161007aa83f
Lea-Ann Sakmann <leaann.sakmann@gmail.com>
2018-04-03 07:06:06 +0200

    feat: change database url to json api

--- dist/sw.js
+++ dist/sw.js
@@ -7,7 +7,6 @@ const STATIC_ASSETS = [
     'js/dbhelper.js',
     'js/main.js',
     'js/restaurant_info.js',
-    'data/restaurants.json',
     'https://fonts.googleapis.com/css?family=Work+Sans:400,500" rel="stylesheet'
 ];
 
@@ -26,17 +25,17 @@ if ('serviceWorker' in navigator) {
 }
 
 // Cache static assets on install
-self.addEventListener('install', (event) => {
+/*self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(STATIC_ASSETS);
         }));
-});
+});*/
 
 // https://jakearchibald.com/2014/offline-cookbook/#on-network-response
 // If the request is in the cache, return it
 // else go to the network, cache the response and return it
-self.addEventListener('fetch', (event) => {
+/*self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.match(event.request).then((response) => {
@@ -47,4 +46,4 @@ self.addEventListener('fetch', (event) => {
             });
         })
     );
-});
+});*/
